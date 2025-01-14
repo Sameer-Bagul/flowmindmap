4e5da0a3356440521427363fdd978785044ae8d5
sameer <sameerbagul2004@gmail.com>
2025-01-14 21:39:17 +0530

    the Shortcuts page linked to the routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
+import Shortcuts from "./pages/Shortcuts";
 
 // Create a client
 const queryClient = new QueryClient({
@@ -23,7 +24,8 @@ function App() {
         <TooltipProvider>
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Index />} />
+            <Route path="/" element={<Index />} />
+            <Route path="/Shortcuts" element={<Shortcuts />} />
             </Routes>
           </BrowserRouter>
           <Toaster />
